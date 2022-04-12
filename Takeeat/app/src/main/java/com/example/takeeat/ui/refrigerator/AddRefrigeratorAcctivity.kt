e59ebd60c872479c240561d306c4a878f8cc1746
wnad <rutc98@naver.com>
2022-04-12 22:19:41 +0900

    add ref item
    추가 버튼 누르면 DB 에 저장.
    입력을 한글로 받으면 오류뜸. 추후에 고칠 예정

--- Takeeat/app/src/main/java/com/example/takeeat/ui/refrigerator/AddRefrigeratorAcctivity.kt
+++ Takeeat/app/src/main/java/com/example/takeeat/ui/refrigerator/AddRefrigeratorAcctivity.kt
@@ -13,6 +13,11 @@ import com.example.takeeat.databinding.ActivityAddrefrigeratorBinding
 import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.DataOutputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
 class AddRefrigeratorActivity :AppCompatActivity() {
     lateinit var binding : ActivityAddrefrigeratorBinding
@@ -40,26 +45,23 @@ class AddRefrigeratorActivity :AppCompatActivity() {
         binding.addrefApplyButton.setOnClickListener {
             //DB추가 여기다 붙여주세요
             //값은 viewmodel.liveData.value(ArrayList<RefItem>타입)을 for문돌려서 추가하면 될거 같아요
-
+            //val jArray = JSONArray()//배열
             // viewmodel -> json 변환
             try {
-                val jArray = JSONArray() //배열
+
                 for (i in 0 until viewmodel.getCount()) {
                     val sObject = JSONObject() //배열 내에 들어갈 json
-                    sObject.put("itemname", viewmodel.liveData.value!![i].itemname)
-                    sObject.put("itemtag", viewmodel.liveData.value!![i].itemtag)
-                    sObject.put("itemexp", viewmodel.liveData.value!![i].itemexp)
-                    sObject.put("itemamount", viewmodel.liveData.value!![i].itemamount)
-                    sObject.put("itemunit", viewmodel.liveData.value!![i].itemunit)
-                    jArray.put(sObject)
+                    sObject.put("item_name", viewmodel.liveData.value!![i].itemname)
+                    //sObject.put("item_tag", viewmodel.liveData.value!![i].itemtag)
+                    sObject.put("item_exdate", viewmodel.liveData.value!![i].itemexp)
+                    sObject.put("item_amount", viewmodel.liveData.value!![i].itemamount)
+                    sObject.put("item_unit", viewmodel.liveData.value!![i].itemunit)
+                    input_ref_item(sObject)
                 }
-                Log.d("JSON Test", jArray.toString())
+                //Log.d("Response", jArray.toString())
             } catch (e: JSONException) {
                 e.printStackTrace()
             }
-
-
-
 
             finish()
         }
@@ -96,6 +98,107 @@ class AddRefrigeratorActivity :AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    fun input_ref_item(job : JSONObject){
+        Thread(Runnable{
+            //handler.post{
+            //try {
+
+            val url: URL = URL("https://b62cvdj81b.execute-api.ap-northeast-2.amazonaws.com/ref-api-test/ref")
+            var conn: HttpURLConnection =url.openConnection() as HttpURLConnection
+            conn.setUseCaches(false)
+            conn.setRequestMethod("POST")
+            //conn.setRequestProperty("Cache-Control", "no-cache");
+            conn.setRequestProperty("Content-Type", "application/json")
+            conn.setRequestProperty("Connection","keep-alive")
+            //conn.setRequestProperty("x-api-key","xL0xZytlwwcGVllGMWN34yrPsaiEbBa5undCLf50")
+            conn.setRequestProperty("Accept", "application/json")
+            conn.setDoOutput(true)
+            conn.setDoInput(true)
+            //conn.connect()
+
+            /*
+            var job = JSONObject()
+
+            job.put("item_name", "안드테스트")
+            job.put("item_amount", "2203")
+            job.put("item_exdate", "22-07-05")
+            job.put("item_unit","g")
+
+             */
+
+
+            var requestBody = job.toString()
+
+
+            Log.d("Response : requestBody = ",requestBody)
+            val wr = DataOutputStream(conn.getOutputStream())
+            wr.writeBytes(requestBody)
+            wr.flush()
+            wr.close()
+
+            var responseCode = conn.getResponseCode()
+            Log.d("Response : responseCode",responseCode.toString())
+            val br: BufferedReader
+            if (responseCode == 200) {
+                br = BufferedReader(InputStreamReader(conn.getInputStream(), "euc-kr"))
+
+                //var re = br.readLine()
+                //Log.d("Response : br.readLine(1) = ",re)
+                //var re1 = br.readLine()
+                //Log.d("Response : br.readLine(2) = ",re1)
+                //var re2 = br.readLine()
+                //Log.d("Response : br.readLine(3) = ",re2)
+                //var re3 = br.readLine()
+                //Log.d("Response : br.readLine() = ",re3)
+                //var re4 = br.readLine()
+                //Log.d("Response : br.readLine() = ",re4)
+
+                //var ree = JSONObject(re)
+                //Log.d("Response : JSONObject(br.readLine()) = ",ree.toString())
+                //var reee = ree.toString()
+                //Log.d("Response : resultJson = ",reee)
+                Log.d("Response","Success")
+            } else {
+                br = BufferedReader(InputStreamReader(conn.getErrorStream(), "euc-kr"))
+                Log.d("Response","fail")
+            }
+
+            /*
+            var re = br.readLine()
+            var ree = JSONObject(re)
+            var reee = ree.toString()
+            Log.d("Response : resultJson = ",reee)
+             */
+
+            var resultJson= JSONObject(br.readLine())
+            //var rrr = br.readLine()
+            Log.d("Response : resultJson = ",resultJson.toString())
+
+            /*
+            var response  = ArrayList<RefItem>()
+            val result = resultJson.get("result")
+            val age = resultJson.get("age");
+            val job = resultJson.get("job");
+            */
+            //Log.i("Response", "DATA response = " + response)
+
+            //conn.disconnect()
+            /*
+            } catch (e:Exception) {
+                Toast.makeText(getApplicationContext(),"데이터 전송 준비 과정 중 오류 발생",Toast.LENGTH_SHORT).show();
+                Log.i("Response", "DATA FAil")
+                return aff;
+            }
+            */
+
+            //}
+
+
+
+
+        }).start()
     }
 
 }
