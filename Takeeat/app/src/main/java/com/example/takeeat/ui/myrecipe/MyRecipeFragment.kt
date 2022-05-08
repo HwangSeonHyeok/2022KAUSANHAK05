4ff7729b23dc57e628dafc1d1a10a50d8b3dfa50
fuckucode <84716853+fuckucode@users.noreply.github.com>
2022-05-08 22:10:21 +0900

    + 눌렀을 때 추가 액티비티 실행
    현재 레이아웃만 나옴. 관련 기능 버그로 수정 중

--- Takeeat/app/src/main/java/com/example/takeeat/ui/myrecipe/MyRecipeFragment.kt
+++ Takeeat/app/src/main/java/com/example/takeeat/ui/myrecipe/MyRecipeFragment.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil.setContentView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.takeeat.R
@@ -72,6 +73,7 @@ class MyRecipeFragment : Fragment() {
 
         return super.onCreateOptionsMenu(menu,inflater)
     }
+
 
     private fun initViewPager() {
         binding.myrecipeContentVp.adapter = MyRecipeVPAdapter(this)
