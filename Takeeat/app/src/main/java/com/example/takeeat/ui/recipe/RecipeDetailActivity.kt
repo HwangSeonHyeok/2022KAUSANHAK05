dac98cbc37a632cbe82f340785a2f0b7ee6eb5ed
wnad <rutc98@naver.com>
2022-05-10 01:38:11 +0900

    재료 수정중
    그냥 커밋함
    재료가 한번에 안불러와짐

--- Takeeat/app/src/main/java/com/example/takeeat/ui/recipe/RecipeDetailActivity.kt
+++ Takeeat/app/src/main/java/com/example/takeeat/ui/recipe/RecipeDetailActivity.kt
@@ -12,6 +12,7 @@ import com.bumptech.glide.Glide
 import com.example.takeeat.R
 import com.example.takeeat.RecipeItem
 import com.example.takeeat.databinding.ActivityRecipedetailBinding
+import com.example.takeeat.ui.refrigerator.RefItem
 import org.json.JSONArray
 import org.json.JSONObject
 import java.io.BufferedReader
@@ -19,6 +20,8 @@ import java.io.DataOutputStream
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
+import java.util.*
+import kotlin.collections.ArrayList
 
 class RecipeDetailActivity : AppCompatActivity() {
 
@@ -29,17 +32,30 @@ class RecipeDetailActivity : AppCompatActivity() {
     lateinit var inMyRefIngre : ArrayList<Int?>
     var writerbookmarked = false
     var recipebookmarked = false
+    var inMyRefItem : ArrayList<RefItem>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityRecipedetailBinding.inflate(layoutInflater)
         recipeItem = intent.getSerializableExtra("Recipe_Data") as RecipeItem
+        ingreAdapter = RecipeDetailIngreAdapter(recipeItem.recipeIngredients)
+        Thread(Runnable {
+            ingreAdapter.recipeMyRefIngreList = get_ingre_myref()
+        }).start()
+        ingreAdapter.recipeID = recipeItem.recipeId
+        binding = ActivityRecipedetailBinding.inflate(layoutInflater)
+
         Glide.with(this).load(recipeItem.imgURL).into(binding.recipedetailMainImage)
         binding.recipedetailRecipeName.text = recipeItem.recipeName
         binding.recipedetailRecipeWriter.text = recipeItem.recipeWriter
         binding.recipedetailRecipeSummary.text = recipeItem.recipeSummary
-        ingreAdapter = RecipeDetailIngreAdapter(recipeItem.recipeIngredients)
+
+
+
+        //ingreAdapter.recipeMyIngreList = get_ingre_myref()
+
+        //Log.d("Responsee = ", getIngreMyref.toString())
         //ingreAdapter.inMyRef = inMyRefIngre
+
         binding.recipedetailIngreRecyclerView.adapter = ingreAdapter
         binding.recipedetailDifficultyText.text = recipeItem.recipeDifficulty
         binding.recipedetailTimeText.text = recipeItem.recipeTime
@@ -95,6 +111,88 @@ class RecipeDetailActivity : AppCompatActivity() {
         setContentView(binding.root)
 
     }
+
+    fun get_ingre_myref() :ArrayList<RefItem>{
+
+        //val handler = Handler()
+        //handler.post{
+        //try {
+
+
+        val recipeMyIngreList = ArrayList<RefItem>()
+
+        val url: URL = URL("https://b62cvdj81b.execute-api.ap-northeast-2.amazonaws.com/ref-api-test//recipe/isingreinmyref")
+        var conn: HttpURLConnection =url.openConnection() as HttpURLConnection
+        conn.setUseCaches(false)
+        conn.setRequestMethod("POST")
+        conn.setRequestProperty("Content-Type", "application/json")
+        conn.setRequestProperty("Connection","keep-alive")
+        conn.setRequestProperty("Accept", "application/json")
+        conn.setDoOutput(true)
+        conn.setDoInput(true)
+
+        var job = JSONObject()
+        job.put("user_id", AWSMobileClient.getInstance().username)
+        job.put("recipe_id", recipeItem.recipeId)
+        var requestBody = job.toString()
+
+
+        Log.d("Response1 = ",requestBody)
+        val wr = DataOutputStream(conn.getOutputStream())
+        wr.writeBytes(requestBody)
+        wr.flush()
+        wr.close()
+
+        val streamReader = InputStreamReader(conn.inputStream)
+        val buffered = BufferedReader(streamReader)
+
+        val content = StringBuilder()
+        while(true) {
+            val line = buffered.readLine() ?: break
+            content.append(line)
+        }
+        val data =content.toString()
+        val jsonArr = JSONArray(data)
+        val i = 0
+        for (i in 0 until jsonArr.length()) {
+            val jsonObj = jsonArr.getJSONObject(i)
+            val datestr: String = jsonObj.getString("item_exdate")
+            var date: Date? = null
+            var tag : String? = null
+            if(datestr != "NULL"){
+                val numm = datestr.split("-")
+                date = Date(numm[0].toInt(),numm[1].toInt()-1,numm[2].toInt())
+            }
+            if(jsonObj.getString("item_tag")=="NULL"){
+                tag = "기타"
+            }else{
+                tag  = jsonObj.getString("item_tag")
+            }
+            recipeMyIngreList.add(
+                RefItem(
+                    jsonObj.getString("item_name"),
+                    tag,
+                    date,
+                    jsonObj.getString("item_amount").toInt(),
+                    jsonObj.getString("item_unit"),
+                    jsonObj.getString("item_id"))
+            )
+            Log.d("Response : jsonObj",jsonObj.toString())
+
+        }
+            //inMyRefItem = recipeMyIngreList
+        // 스트림과 커넥션 해제
+        buffered.close()
+        conn.disconnect()
+        //Log.d("Responsee : ",recipeMyIngreList.toString())
+
+
+        return recipeMyIngreList
+
+
+
+    }
+
     fun bookmark_on(bookmarkId: String) {
         val handler = Handler()
         Thread(Runnable {
--- Takeeat/app/src/main/java/com/example/takeeat/ui/recipe/RecipeDetailIngreAdapter.kt
+++ Takeeat/app/src/main/java/com/example/takeeat/ui/recipe/RecipeDetailIngreAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.takeeat.ui.recipe
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -7,23 +8,35 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import androidx.room.Room
+import com.amazonaws.mobile.client.AWSMobileClient
 import com.example.takeeat.AppDatabase
 import com.example.takeeat.IngredientsInfo
 import com.example.takeeat.R
 import com.example.takeeat.ShoppingListItem
+import com.example.takeeat.ui.refrigerator.RefItem
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.DataOutputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import java.util.*
 
 import kotlin.collections.ArrayList
 
 class RecipeDetailIngreAdapter(var data: ArrayList<IngredientsInfo>):  RecyclerView.Adapter<RecipeDetailIngreAdapter.ViewHolder>() {
     lateinit var db : AppDatabase
     lateinit var inMyRef : ArrayList<Int?>
+    public var recipeID = "1"
+    public var recipeMyRefIngreList : ArrayList<RefItem>? = null
+    var check : Int = 0
 
     inner class ViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_recipeingre, parent, false)) {
         val ingreName: TextView
         val ingreCount: TextView
         val ingreInMyRef: TextView
         val addShoppingListButton : ImageView
-
 
         init {
             db = Room.databaseBuilder(parent.context,AppDatabase::class.java,"shoppinglist").allowMainThreadQueries().build()
@@ -49,6 +62,20 @@ class RecipeDetailIngreAdapter(var data: ArrayList<IngredientsInfo>):  RecyclerV
                 else
                     ingreCount.text = item.ingreUnit
                 ingreInMyRef.text = "0개" // user냉장고 안에 ingreName 태그이 일치하는 품목이 있는지 확인하고 그 수의 합을 여기 저장
+                if(recipeMyRefIngreList!=null){
+                    for (i in 0 until recipeMyRefIngreList!!.size) {
+                        if(ingreName.text.contains(recipeMyRefIngreList!!.get(i).itemtag!!)){
+                            ingreInMyRef.text = recipeMyRefIngreList!!.get(i).itemamount.toString() + recipeMyRefIngreList!!.get(i).itemunit
+                            //inMyRef!!.add(recipeMyRefIngreList!!.get(i).itemid!!.toInt())
+                            check = 1
+                        }
+
+                    }
+                }
+                if(check!=1){
+                    //inMyRef!!.add(0)
+                }
+                check = 0
                 addShoppingListButton.setOnClickListener {
                     var dbitem : ShoppingListItem? =null
                     var toastvalue : String = item.ingreName
@@ -75,5 +102,86 @@ class RecipeDetailIngreAdapter(var data: ArrayList<IngredientsInfo>):  RecyclerV
     override fun getItemCount(): Int {
         return data.size
     }
+
+    /*
+    fun get_ingre_myref(){
+        Thread(Runnable {
+        //val handler = Handler()
+        //handler.post{
+        //try {
+
+
+        val recipeMyIngreList = ArrayList<RefItem>()
+
+        val url: URL = URL("https://b62cvdj81b.execute-api.ap-northeast-2.amazonaws.com/ref-api-test//recipe/isingreinmyref")
+        var conn: HttpURLConnection =url.openConnection() as HttpURLConnection
+        conn.setUseCaches(false)
+        conn.setRequestMethod("POST")
+        conn.setRequestProperty("Content-Type", "application/json")
+        conn.setRequestProperty("Connection","keep-alive")
+        conn.setRequestProperty("Accept", "application/json")
+        conn.setDoOutput(true)
+        conn.setDoInput(true)
+
+        var job = JSONObject()
+        job.put("user_id", AWSMobileClient.getInstance().username)
+        job.put("recipe_id", recipeID)
+        var requestBody = job.toString()
+
+
+        Log.d("Response1 = ",requestBody)
+        val wr = DataOutputStream(conn.getOutputStream())
+        wr.writeBytes(requestBody)
+        wr.flush()
+        wr.close()
+
+        val streamReader = InputStreamReader(conn.inputStream)
+        val buffered = BufferedReader(streamReader)
+
+        val content = StringBuilder()
+        while(true) {
+            val line = buffered.readLine() ?: break
+            content.append(line)
+        }
+        val data =content.toString()
+        val jsonArr = JSONArray(data)
+        val i = 0
+        for (i in 0 until jsonArr.length()) {
+            val jsonObj = jsonArr.getJSONObject(i)
+            val datestr: String = jsonObj.getString("item_exdate")
+            var date: Date? = null
+            var tag : String? = null
+            if(datestr != "NULL"){
+                val numm = datestr.split("-")
+                date = Date(numm[0].toInt(),numm[1].toInt()-1,numm[2].toInt())
+            }
+            if(jsonObj.getString("item_tag")=="NULL"){
+                tag = "기타"
+            }else{
+                tag  = jsonObj.getString("item_tag")
+            }
+            recipeMyIngreList.add(
+                RefItem(
+                    jsonObj.getString("item_name"),
+                    tag,
+                    date,
+                    jsonObj.getString("item_amount").toInt(),
+                    jsonObj.getString("item_unit"),
+                    jsonObj.getString("item_id"))
+            )
+            Log.d("Response : jsonObj",jsonObj.toString())
+
+        }
+
+        // 스트림과 커넥션 해제
+        buffered.close()
+        conn.disconnect()
+        //Log.d("Responsee : ",recipeMyIngreList.toString())
+        recipeMyRefIngreList = recipeMyIngreList
+        }).start()
+
+    }
+
+     */
 
 }
