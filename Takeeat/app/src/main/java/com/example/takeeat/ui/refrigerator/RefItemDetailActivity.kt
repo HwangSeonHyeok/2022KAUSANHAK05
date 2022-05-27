a3f61c6e1bb627d6e2f134e17651ef5c34a7855c
wnad <rutc98@naver.com>
2022-05-27 16:41:42 +0900

    DB 수정으로 냉장고 update delete 안되던 오류 수정

--- Takeeat/app/src/main/java/com/example/takeeat/ui/refrigerator/RefItemDetailActivity.kt
+++ Takeeat/app/src/main/java/com/example/takeeat/ui/refrigerator/RefItemDetailActivity.kt
@@ -99,16 +99,13 @@ class RefItemDetailActivity : AppCompatActivity() {
 
             //여기다 DB업데이트 코드 넣어주세요 refItem값으로 업데이트 해주면됩니다
             val sObject = JSONObject() //배열 내에 들어갈 json
-            sObject.put("id", AWSMobileClient.getInstance().username+refItem.itemid.toString())
+            sObject.put("id", AWSMobileClient.getInstance().username+"item:"+refItem.itemid.toString())
             sObject.put("item_id", refItem.itemid.toString())
             sObject.put("update_name", URLEncoder.encode(refItem.itemname.toString(), "UTF-8"))
             sObject.put("update_amount", refItem.itemamount.toString())
             sObject.put("update_exdate", binding.refDetailEXP.text.toString().replace(".", "-"))
             sObject.put("update_tag", URLEncoder.encode(refItem.itemtag, "UTF-8"))
             sObject.put("update_unit", URLEncoder.encode(refItem.itemunit.toString(), "UTF-8"))
-
-
-            //Log.d("Response sObjects sObject sObject sObject : ",sObject.toString())
 
 
             update_ref_item(sObject)
@@ -128,7 +125,6 @@ class RefItemDetailActivity : AppCompatActivity() {
             dialogBuilder.setItems(tags) {
                     p0, p1 ->
                 binding.refDetailTag.text = tags[p1]
-                //refItem.itemtag = tags[p1]
 
             }
             val alertDialog = dialogBuilder.create()
@@ -138,17 +134,6 @@ class RefItemDetailActivity : AppCompatActivity() {
         binding.refDetailEXP.isClickable = false
         binding.refDetailTag.isClickable = false
 
-
-        /*val JA = JSONArray("[{\"ingre_name\":\"계란\",\"ingre_num\":1,\"ingre_count\":\"5\",\"ingre_unit\":\"개\"},{\"ingre_name\":\"육수팩\",\"ingre_num\":2,\"ingre_count\":\"1\",\"ingre_unit\":\"개\"},{\"ingre_name\":\"소금\",\"ingre_num\":3,\"ingre_count\":\"\",\"ingre_unit\":\"약간\"}]")
-
-        recipeArray.add(RecipeItem("1","집에서도 쉽게 찰떡과 조청으로 만든 꿀떡 만드는법",JA,
-            "찹쌀과 조청으로 집에서도 쉽고 빠르게드실 수 있는 찹쌀 꿀떡을 만들어봤습니다그럼 저희 영상을 봐주시고 채널을 들려주셔서 감사합니다",
-            4.2,"120","중급",null, URL("https://recipe1.ezmember.co.kr/cache/recipe/2022/02/17/1f40ef46386de280a5d80601d0d39ae01.jpg")
-        ))
-        recipeArray.add(RecipeItem("2","단짠단짠의 대패덮밥",JA,
-            "뜨끈한 밥에 대패삼겹살 한점!집밥백선생 강추레시피! 간단하고 빠르게 만드는 별미메뉴!입맛없을때 만들먹으면 밥두공기도 거뜬해요.",
-            4.6,"30","초급",null, URL("https://recipe1.ezmember.co.kr/cache/recipe/2017/10/22/3211f299a02729bc2d05649ceec734771.jpg")
-        ))*/
 
 
         progressOFF()
@@ -204,12 +189,9 @@ class RefItemDetailActivity : AppCompatActivity() {
                 R.id.refitemdetail_deletebutton -> {
                     //여기다 db삭제 코드 만들어 주세요
                     val sObject = JSONObject() //배열 내에 들어갈 json
-                    sObject.put("id", AWSMobileClient.getInstance().username+refItem.itemid.toString())
+                    sObject.put("id", AWSMobileClient.getInstance().username+"item:"+refItem.itemid.toString())
                     sObject.put("item_id", refItem.itemid.toString())
 
-
-
-                    //Log.d("Response sObjects sObject sObject sObject : ",sObject.toString())
 
                     delete_ref_item(sObject)
 
@@ -225,8 +207,6 @@ class RefItemDetailActivity : AppCompatActivity() {
 
     fun update_ref_item(job : JSONObject){
         Thread(Runnable{
-            //handler.post{
-            //try {
             AWSMobileClient.getInstance()
 
             val url: URL = URL("https://b62cvdj81b.execute-api.ap-northeast-2.amazonaws.com/ref-api-test/ref/update")
@@ -254,63 +234,6 @@ class RefItemDetailActivity : AppCompatActivity() {
 
             var responseCode = conn.getResponseCode()
             Log.d("Response : responseCode",responseCode.toString())
-            val br: BufferedReader
-            if (responseCode == 200) {
-                br = BufferedReader(InputStreamReader(conn.getInputStream()))
-
-                //var re = br.readLine()
-                //Log.d("Response : br.readLine(1) = ",re)
-                //var re1 = br.readLine()
-                //Log.d("Response : br.readLine(2) = ",re1)
-                //var re2 = br.readLine()
-                //Log.d("Response : br.readLine(3) = ",re2)
-                //var re3 = br.readLine()
-                //Log.d("Response : br.readLine() = ",re3)
-                //var re4 = br.readLine()
-                //Log.d("Response : br.readLine() = ",re4)
-
-                //var ree = JSONObject(re)
-                //Log.d("Response : JSONObject(br.readLine()) = ",ree.toString())
-                //var reee = ree.toString()
-                //Log.d("Response : resultJson = ",reee)
-                Log.d("Response","Success Success Success Success Success Success")
-            } else {
-                br = BufferedReader(InputStreamReader(conn.getErrorStream()))
-                Log.d("Response","fail")
-            }
-
-            /*
-            var re = br.readLine()
-            var ree = JSONObject(re)
-            var reee = ree.toString()
-            Log.d("Response : resultJson = ",reee)
-             */
-
-            //var resultJson= JSONObject(br.readLine())
-            //var rrr = br.readLine()
-            //Log.d("Response : resultJson = ",resultJson.toString())
-
-            /*
-            var response  = ArrayList<RefItem>()
-            val result = resultJson.get("result")
-            val age = resultJson.get("age");
-            val job = resultJson.get("job");
-            */
-            //Log.i("Response", "DATA response = " + response)
-
-            //conn.disconnect()
-            /*
-            } catch (e:Exception) {
-                Toast.makeText(getApplicationContext(),"데이터 전송 준비 과정 중 오류 발생",Toast.LENGTH_SHORT).show();
-                Log.i("Response", "DATA FAil")
-                return aff;
-            }
-            */
-
-            //}
-
-
-
 
         }).start()
     }
@@ -318,89 +241,39 @@ class RefItemDetailActivity : AppCompatActivity() {
     fun delete_ref_item(job : JSONObject){
         val handler = Handler()
         Thread(Runnable{
-            //handler.post{
-            //try {
+
             AWSMobileClient.getInstance()
 
             val url: URL = URL("https://b62cvdj81b.execute-api.ap-northeast-2.amazonaws.com/ref-api-test/ref/delete")
             var conn: HttpURLConnection =url.openConnection() as HttpURLConnection
             conn.setUseCaches(false)
             conn.setRequestMethod("POST")
-            //conn.setRequestProperty("Cache-Control", "no-cache");
             conn.setRequestProperty("Content-Type", "application/json")
             conn.setRequestProperty("Connection","keep-alive")
-            //conn.setRequestProperty("x-api-key","xL0xZytlwwcGVllGMWN34yrPsaiEbBa5undCLf50")
             conn.setRequestProperty("Accept", "application/json")
             conn.setDoOutput(true)
             conn.setDoInput(true)
-            //conn.connect()
-
 
             var requestBody = job.toString()
+            Log.d("Responseeee : requestBody", requestBody)
 
 
-            Log.d("Response1 = ",requestBody)
             val wr = DataOutputStream(conn.getOutputStream())
             wr.writeBytes(requestBody)
             wr.flush()
             wr.close()
 
             var responseCode = conn.getResponseCode()
-            Log.d("Response : responseCode",responseCode.toString())
+
             val br: BufferedReader
             if (responseCode == 200) {
                 br = BufferedReader(InputStreamReader(conn.getInputStream()))
-
-                //var re = br.readLine()
-                //Log.d("Response : br.readLine(1) = ",re)
-                //var re1 = br.readLine()
-                //Log.d("Response : br.readLine(2) = ",re1)
-                //var re2 = br.readLine()
-                //Log.d("Response : br.readLine(3) = ",re2)
-                //var re3 = br.readLine()
-                //Log.d("Response : br.readLine() = ",re3)
-                //var re4 = br.readLine()
-                //Log.d("Response : br.readLine() = ",re4)
-
-                //var ree = JSONObject(re)
-                //Log.d("Response : JSONObject(br.readLine()) = ",ree.toString())
-                //var reee = ree.toString()
-                //Log.d("Response : resultJson = ",reee)
                 Log.d("Response","Success Success Success Success Success Success")
             } else {
                 br = BufferedReader(InputStreamReader(conn.getErrorStream()))
                 Log.d("Response","fail")
             }
 
-            /*
-            var re = br.readLine()
-            var ree = JSONObject(re)
-            var reee = ree.toString()
-            Log.d("Response : resultJson = ",reee)
-             */
-
-            //var resultJson= JSONObject(br.readLine())
-            //var rrr = br.readLine()
-            //Log.d("Response : resultJson = ",resultJson.toString())
-
-            /*
-            var response  = ArrayList<RefItem>()
-            val result = resultJson.get("result")
-            val age = resultJson.get("age");
-            val job = resultJson.get("job");
-            */
-            //Log.i("Response", "DATA response = " + response)
-
-            //conn.disconnect()
-            /*
-            } catch (e:Exception) {
-                Toast.makeText(getApplicationContext(),"데이터 전송 준비 과정 중 오류 발생",Toast.LENGTH_SHORT).show();
-                Log.i("Response", "DATA FAil")
-                return aff;
-            }
-            */
-
-            //}
 
             handler.post{
                 Toast.makeText(this, "삭제되었습니다", Toast.LENGTH_LONG).show()
